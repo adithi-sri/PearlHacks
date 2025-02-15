ae7f55e9699ff069015d48fd41f24b84f72ab874
olivia <aiolivia@ad.unc.edu>
2025-02-15 16:05:21 -0500

    h

--- pearlhacks-app/src/App.tsx
+++ pearlhacks-app/src/App.tsx
@@ -1,9 +1,8 @@
 import Login from "./login";
-import ListGroup from "./components/ListGroup";
-import Message from "./Message"
 
 function App() {
-  return <div><Message></Message></div>;
+  <Message></Message>
+  return <Login></Login>;
 }
 
 export default App;
