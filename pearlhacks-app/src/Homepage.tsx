7aad4d73331fa47c002108a045c4cb32bab16ed0
olivia <aiolivia@ad.unc.edu>
2025-02-16 09:45:42 -0500

    hh

--- pearlhacks-app/src/Homepage.tsx
+++ pearlhacks-app/src/Homepage.tsx
@@ -6,10 +6,12 @@ import Login from "./components/login";
 import Register from "./components/Register";
 import "./App.css";
 import Timer from "./components/Timer";
+import "./Homepage.css";
 
 function Homepage() {
   return (
     <div>
+      <h1 className="tim-heading">Study Timer</h1>
       <Timer></Timer>
       <Leaderboard />
       <User></User>
