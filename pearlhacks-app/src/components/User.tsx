18bc099e8e30828a6d56dbf6ce145e0c351fff79
olivia <aiolivia@ad.unc.edu>
2025-02-16 09:10:41 -0500

    trial

--- pearlhacks-app/src/components/User.tsx
+++ pearlhacks-app/src/components/User.tsx
@@ -3,7 +3,13 @@ import "./User.css";
 function User() {
   return (
     <div className="profile-container">
-      <h1 className="user">Jane Doe</h1>
+      <h3 className="user">Jane Doe</h3>
+      <p className="writingstyles">
+        I am a freshman at UNC trying to find a study buddy. I am double
+        majoring in neuroscience and psychology. I live on North Campus, so if
+        anyone nearby is taking similar classes, please reach out!{" "}
+      </p>
+      <h3 className="ranking">Leaderboard Ranking: 3rd</h3>
     </div>
   );
 }
