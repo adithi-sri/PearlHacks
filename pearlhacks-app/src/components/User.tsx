6eaca5f00627db49e8179e8a90c743938b4ffe1f
olivia <aiolivia@ad.unc.edu>
2025-02-16 10:18:38 -0500

    yes

--- pearlhacks-app/src/components/User.tsx
+++ pearlhacks-app/src/components/User.tsx
@@ -1,6 +1,16 @@
 import "./User.css";
 
 function User() {
+  const items = [
+    "Monday - 2.5 hours",
+    "Tuesday - 3.3 hours",
+    "Wednesday - 4.44 hours",
+    "Thursday - 1 hour",
+    "Friday - 0.55 hours",
+    "Saturday - 3.2 hours",
+    "Sunday - 4 hours",
+  ];
+
   return (
     <div className="profile-container">
       <h3 className="user">Jane Doe</h3>
@@ -10,9 +20,15 @@ function User() {
         anyone nearby is taking similar classes, please reach out!{" "}
       </p>
       <h3 className="ranking">Leaderboard Ranking: 3rd</h3>
+      <h3 className="ranking">Weekly Points: 1,300</h3>
       <button className="chat">Chat</button>
       <button className="search">Search students</button>
       <h1 className="week-progress">Weekly Progress</h1>
+      <ul className="week-hours">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
